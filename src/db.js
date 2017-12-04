5066def24e2ed4ffdc51809d0fd8b7f5dd456acb
harrison-symes <symeshjb@gmail.com>
2017-12-04 17:11:39 +1300

    list headers and colorised cohorts

--- src/db.js
+++ src/db.js
@@ -1,10 +1,20 @@
 const config = require('../knexfile').development
 const knex = require('knex')(config)
 
-function fill (string, len) {
+const colors = require('colors')
+
+function f (string, len, colour) {
   let size = len- string.toString().length
   if (size < 0) size = 0
-  return `${string}${Array(size).fill(" ").join('')}`
+  return colors[colour || 'white'](`${string}${Array(size).fill(" ").join('')}`)
+}
+
+function printStudent ({name, last_name, cohort_name, current_sprint, github_name, cl_colour}) {
+  return console.log(`${f(name, 15)}${f(last_name, 15)} ${f(cohort_name, 15, cl_colour)}${f(current_sprint, 5)} @${f(github_name, 20)}`)
+}
+
+function headers () {
+  return console.log(`${f("name", 15, "underline")}${f("last name", 15, "underline")} ${f("cohortname", 15, "underline")}${f("sprnt", 5, "underline")} @${f("githubname", 20, "underline")}`)
 }
 
 module.exports = {
@@ -26,6 +36,7 @@ module.exports = {
     .join('cohorts', 'students.cohort_id', 'like', 'cohorts.id')
     .orderBy('name', 'asc')
     .then(students => {
-      students.forEach(({name, last_name, cohort_name, current_sprint, github_name}) => console.log(`${fill(name, 15)}${fill(last_name, 15)}${fill(cohort_name, 15)}${fill(current_sprint, 5)} @${fill(github_name, 20)}`))
+      headers()
+      students.forEach(printStudent)
     })
 }
