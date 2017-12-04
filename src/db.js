a2458480d5e8b072e0419859f4780fbe0b313e82
harrison-symes <symeshjb@gmail.com>
2017-12-04 17:53:33 +1300

    updates last sprint update col when pushing sprint

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ const config = require('../knexfile').development
 const knex = require('knex')(config)
 
 const moment = require('moment')
+const {tz} = require('moment-timezone')
 const colors = require('colors')
 
 function flag (last_update) {
@@ -21,7 +22,7 @@ function f (string, len, colour) {
 }
 
 function printStudent ({name, last_name, cohort_name, current_sprint, github_name, cl_colour, last_update}) {
-  return console.log(`${f(name, 15)}${f(last_name, 15)} ${f(cohort_name, 15, cl_colour)}${f(current_sprint, 5)} @${f(github_name, 20)} ${flag(last_update)}`)
+  return console.log(`${f(name, 15)}${f(last_name, 15)} ${f(cohort_name, 15, cl_colour)}${f(current_sprint, 5)} @${f(github_name, 30)} ${flag(last_update)}`)
 }
 
 function headers () {
@@ -37,7 +38,7 @@ module.exports = {
   addStudent: (student) => knex('students')
     .insert(student),
   updateSprint: (github_name, current_sprint) => knex('students')
-    .update({current_sprint})
+    .update({current_sprint, last_update: moment(knex.fn.now()).tz('Pacific/Auckland').format()})
     .where('students.github_name', github_name),
   deleteStudent: (github_name) => knex('students')
     .where('github_name', github_name)
