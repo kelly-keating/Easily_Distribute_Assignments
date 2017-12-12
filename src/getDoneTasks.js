a5f7841cd33d9ac07ef7b155bc1d04fa6f4f03d0
harrison-symes <symeshjb@gmail.com>
2017-12-13 12:00:10 +1300

    can close issues

--- src/getDoneTasks.js
+++ src/getDoneTasks.js
@@ -1,14 +1,45 @@
-const github = require('octonode')
+const github = require('octonode2')
 var client = github.client(process.env['WTR_ACCESS_TOKEN'])
 
-const repo = client.repo('kahu-2018/kahu-2018')
 
+closeDone(1, 'harakeke-2017/harakeke-2017')
+  .then(() => closeDone(1, 'kahu-2018/kahu-2018'))
+  .then(() => console.log("done"))
+  .catch((err) => console.log(err))
 
-repo.issues({page: 1, per_page: 300, state: 'open'}, (err, data) => {
-  // console.log(data[0]);
-  // console.log({err, data});
-  // console.log(data[0]);
-  // data.forEach(item => console.log(item.labels))
-  let doneTasks = data.filter(item => item.labels.find(label => label.name == 'done')).map(done => done.id)
-  console.log(doneTasks);
-})
+function closeDone(page, repo) {
+  return new Promise(function(resolve, reject) {
+    if (page > 50) resolve()
+    else {
+      const ghrepo = client.repo(repo)
+      console.log("page", page);
+      ghrepo.issues({page: page, per_page: 100, state: 'open'}, (err, data) => {
+        // console.log(data[0]);
+        console.log({err});
+        if (err) resolve('done')
+        // console.log({err, data});
+        // console.log(data[0]);
+        // data.forEach(item => console.log(item.labels))
+        let doneTasks = data.filter(item => item.labels.find(label => label.name == 'done')).map(done => done.number)
+        console.log(doneTasks)
+        closeIssues(doneTasks, 0, repo)
+        .then(() => resolve(closeDone(page + 1, repo)))
+        .catch(() => closeDone(page + 1, repo))
+      })
+    }
+  });
+}
+
+function closeIssues(arr, i, repo) {
+  return new Promise(function(resolve, reject) {
+    if (i >= arr.length) resolve('done')
+    else {
+      const ghissue = client.issue(repo, arr[i])
+      console.log("closing", arr[i]);
+      ghissue.update({state: 'closed'}, (err, data) => {
+        console.log({err});
+        resolve(closeIssues(arr, i + 1, repo))
+      })
+    }
+  });
+}
