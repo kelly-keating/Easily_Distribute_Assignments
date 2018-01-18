fcb3b587d7e9d62a4c237f98d6035cb4fcc4481a
harrison-symes <symeshjb@gmail.com>
2018-01-19 12:19:33 +1300

    working

--- src/getDoneTasks.js
+++ src/getDoneTasks.js
@@ -2,15 +2,16 @@ const github = require('octonode2')
 var client = github.client(process.env['WTR_ACCESS_TOKEN'])
 
 module.exports = () => {
-  return closeDone(1, 'phase-0/harakeke-2017')
+  return closeDone(1, 'phase-0/phase-0')
   .then(() => closeDone(1, 'phase-0/kahu-2018'))
+  .then(() => closeDone(1, 'phase-0/harakeke-2017'))
   .then(() => console.log("done"))
   .catch((err) => console.log(err))
 }
 
 function closeDone(page, repo) {
   return new Promise(function(resolve, reject) {
-    if (page > 30) resolve()
+    if (page > 15) resolve()
     else {
       const ghrepo = client.repo(repo)
       console.log("page", page);
@@ -21,7 +22,7 @@ function closeDone(page, repo) {
         // console.log({err, data});
         // console.log(data[0]);
         // data.forEach(item => console.log(item.labels))
-        let doneTasks = data.filter(item => item.labels.find(label => label.name == 'done')).map(done => done.number)
+        let doneTasks = data.filter(item => item.labels.find(label => label.name == 'done')||(item.assignee && item.assignee.login == 'harrison-symes')).map(done => done.number)
         console.log(doneTasks)
         closeIssues(doneTasks, 0, repo)
         .then(() => resolve(closeDone(page + 1, repo)))
