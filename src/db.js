b838f2e322a025ed90de6afebb2d23a46639d79c
harrison-symes <symeshjb@gmail.com>
2018-05-30 11:29:56 +1200

    Refactor and alphabet sorter

--- src/db.js
+++ src/db.js
@@ -22,11 +22,26 @@ function f (string, len, colour) {
 }
 
 function printStudent ({name, last_name, cohort_name, current_sprint, github_name, cl_colour, last_update}) {
-  return console.log(`${f(name, 15)}${f(last_name, 15)} ${f(cohort_name, 20, cl_colour)}${f(current_sprint, 5)} @${f(github_name, 30)} ${flag(last_update)}`)
+  return console.log(`${f(name, 15)}${f(last_name, 15)} ${f(current_sprint, 5)} @${f(github_name, 30)} ${flag(last_update)}`)
 }
 
 function headers () {
-  return console.log(`${f("name", 15, "underline")}${f("last name", 15, "underline")} ${f("cohortname", 20, "underline")}${f("sprnt", 5, "underline")} @${f("githubname", 20, "underline")}`)
+  return console.log(`${f("name", 15, "underline")}${f("last name", 15, "underline")} ${f("sprnt", 5, "underline")} @${f("githubname", 20, "underline")}`)
+}
+
+function alphabetSorter (students) {
+  var sorted = []
+
+  students.forEach(student => {
+    if (sorted.length == 0) return sorted.push(student)
+
+    for (var i = 0; i < sorted.length; i++) {
+      if (student.name.toLowerCase() < sorted[i].name.toLowerCase()) return sorted.splice(i, 0, student)
+    }
+    sorted.push(student)
+  })
+
+  return sorted
 }
 
 module.exports = {
@@ -49,7 +64,12 @@ module.exports = {
     .orderBy('current_sprint', 'asc')
     .then(students => {
       headers()
-      students.forEach(printStudent)
-      console.log(`Students in system: ${students.length}`);
+
+      alphabetSorter(students).forEach(printStudent)
+      const active = students.filter(student => {
+        const fromNow = moment(student.last_update).tz('Pacific/Auckland').fromNow()
+        return !fromNow.includes('months')
+      }).length
+      console.log(`Students in system: ${students.length} - ${active} Active`);
     })
 }
